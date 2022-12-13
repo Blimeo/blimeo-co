5610050ab939b68fd08629d14ae27020b361d02c
Matthew Ye <matthewgeography@gmail.com>
2022-12-12 21:38:01 -0800

    sadfaf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto_Mono } from "@next/font/google";
-const roboto = Roboto_Mono({ subsets: ["latin"] });
+const roboto = Roboto_Mono({subsets: ["latin"] , preload: true});
 
 
 export default function App({ Component, pageProps }: AppProps) {
